bb9b64c964a8b2a1ad821230a7434385b48d3b25
Rama Venkata Manikanta Sairam Kattunga <112626004+Sairam-kattunga@users.noreply.github.com>
2025-07-16 22:03:37 +0530

    Calci

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-function updateClock(){
-    const now = new Date();
-    let hours = now.getHours();
 
-    const meridian = hours > 12?"PM":"AM";
-    hours = hours % 12 || 12;
+const display = document.getElementById("display");
+function appendToDisplay(input){
+    display.value +=input;
+}
+function clearDisplay(){
+   display.value="";   
+}
+function calculate(){
+    try{
+        display.value = eval(display.value);
 
-    hours = hours.toString().padStart(2,0);
-    const minutes = now.getMinutes().toString().padStart(2,0);
-    const seconds = now.getSeconds().toString().padStart(2,0);
-
-    const timeString = `${hours}:${minutes}:${seconds} ${meridian}`;
-
-    document.getElementById("clock").textContent = timeString;
+    }
+    catch(error){
+        display.value = "Error";
+       }
 
 }
-updateClock();
-setInterval(updateClock,1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
